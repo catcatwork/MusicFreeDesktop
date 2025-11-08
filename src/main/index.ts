1e500ac961d7fa34fef79f731b2c32e316a37f57
maotoumao <lhx_xjtu@163.com>
2025-11-08 22:49:12 +0800

    fix: 修复打开软件时可能出现白框的问题

--- src/main/index.ts
+++ src/main/index.ts
@@ -51,6 +51,7 @@ if (process.defaultApp) {
 } else {
     app.setAsDefaultProtocolClient("musicfree");
 }
+app.commandLine.appendSwitch("--disable-gpu-sandbox");
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
